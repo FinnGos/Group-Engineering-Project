dd087dba7c9588850be311ab43374c8f2251240d
dight <dight@LAPTOP-U1HTPV9Q>
2025-02-14 11:56:53 +0000

    Tests have been merged, 4 failures resolve needed

--- SustainableApplication/home/urls.py
+++ SustainableApplication/home/urls.py
@@ -5,6 +5,6 @@ from django.shortcuts import redirect
 
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.home, name="home"),
     path("signup/", views.signup, name="signup"),
 ]
