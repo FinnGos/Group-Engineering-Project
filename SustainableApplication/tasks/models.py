5848dbf1abc2705670caee08c6c3b0de0c843bf0
DCook18 <danieladam.cook@icloud.com>
2025-02-19 20:11:50 +0000

    Creating and filling database with basic row

--- SustainableApplication/tasks/models.py
+++ SustainableApplication/tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Tasks(models.Model):
+    task_name = models.CharField(max_length=200)
+    current_progress = models.IntegerField(default=0)
+    target = models.IntegerField(default=0)
+    reward = models.IntegerField(default=0)
