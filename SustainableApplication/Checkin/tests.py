e219592a2f045ea9de5e55117a0b20266407df73
JacobNixon2004 <jake.rob.nixon@gmail.com>
2025-02-19 16:54:21 +0000

    Update tests.py

--- SustainableApplication/Checkin/tests.py
+++ SustainableApplication/Checkin/tests.py
@@ -14,18 +14,24 @@ class ViewsTestCase(TestCase):
 
     def test_get_location_missing_params(self):
         """
-        Tests
+        Tests initial load up of page where no location is given
         """
         response = self.client.get(reverse('get_location'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'checkin_page.html')
 
     def test_get_location_missing_lat(self):
+        """
+        Tests when only longitude is given
+        """
         response = self.client.get(reverse('get_location'), {'lon': '-74.0060'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'checkin_page.html')
 
     def test_get_location_missing_lon(self):
+        """
+        Tests when only latitude is given
+        """
         response = self.client.get(reverse('get_location'), {'lat': '40.7128'})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'checkin_page.html')
@@ -36,28 +42,43 @@ class ViewsTestCase(TestCase):
         self.assertTemplateUsed(response, 'checkin_page.html')
 
     def test_get_location_invalid_values(self):
+        """
+        Tests if an invalid type is given
+        """
         response = self.client.get(reverse('get_location'), {'lat': 'abc', 'lon': 'xyz'})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.content.decode(), 'Invalid location data.')
 
     def test_get_location_maximum_values(self):
+        """
+        Tests the edge cases of maximum
+        """
         response = self.client.get(reverse('get_location'), {'lat': '90.0', 'lon': '180.0'})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Check-In Successful! Lat: 90.0, Lon: 180.0')
         self.assertTemplateUsed(response, 'checkin_page.html')
 
     def test_get_location_minimum_values(self):
+        """
+        Tests the edge of minimum
+        """
         response = self.client.get(reverse('get_location'), {'lat': '-90.0', 'lon': '-180.0'})
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Check-In Successful! Lat: -90.0, Lon: -180.0')
         self.assertTemplateUsed(response, 'checkin_page.html')
 
     def test_get_location_above_maximum_values(self):
+        """
+        Tests going over maximum
+        """
         response = self.client.get(reverse('get_location'), {'lat': '90.1', 'lon': '180.1'})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.content.decode(), 'Invalid location data.')
 
     def test_get_location_below_minimum_values(self):
+        """
+        Tests going over minimum
+        """
         response = self.client.get(reverse('get_location'), {'lat': '-90.1', 'lon': '-180.1'})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.content.decode(), 'Invalid location data.')
