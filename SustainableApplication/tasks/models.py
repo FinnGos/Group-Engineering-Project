db853fcd2c268c6231d4d94cf31ab2dd9567a56e
DCook18 <danieladam.cook@icloud.com>
2025-02-19 21:23:50 +0000

    Finished html and starting database

--- SustainableApplication/tasks/models.py
+++ SustainableApplication/tasks/models.py
@@ -7,3 +7,15 @@ class Tasks(models.Model):
     current_progress = models.IntegerField(default=0)
     target = models.IntegerField(default=0)
     reward = models.IntegerField(default=0)
+    completed = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        self.completed = self.current_progress == self.target
+        super().save(*args, **kwargs)
+
+    @property
+    def progress_percentage(self):
+        return (self.current_progress / self.target) * 100 if self.target > 0 else 0
+
+    def __str__(self):
+        return self.task_name
--- SustainableApplication/tasks/views.py
+++ SustainableApplication/tasks/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
+from .models import Tasks
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
 def tasks_view(request):
-    context = {}
+    incomplete_tasks = Tasks.objects.filter(completed=False)
 
-    return render(request, "tasks.html", context)
+    return render(request, "tasks.html", {"tasks": incomplete_tasks})
+
+
+def update_progress(request, task_id, action):
+    task = get_object_or_404(Tasks, id=task_id)
+
+    if action == "increase" and task.current_progress < task.target:
+        task.current_progress += 1
+    elif action == "decrease" and task.current_progress > 0:
+        task.current_progress -= 1
+    else:
+        return JsonResponse(
+            {"success": False, "message": "Invalid action or limit reached."}
+        )
+
+    task.save()
+    return JsonResponse(
+        {
+            "success": True,
+            "new_progress": task.progress_percentage,
+            "current_progress": task.current_progress,
+            "target": task.target,
+        }
+    )
