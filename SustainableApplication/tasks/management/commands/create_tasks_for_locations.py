d5f57c3f1837f0deb8b89d523ab332ec4d6f38db
DCook18 <danieladam.cook@icloud.com>
2025-03-03 14:05:29 +0000

    made create_tasks_for_locations that allows us to sync Locations with tasks at any time

---
+++ SustainableApplication/tasks/management/commands/create_tasks_for_locations.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand
+from tasks.models import Tasks
+from Checkin.models import Location
+
+
+class Command(BaseCommand):
+    """Allows us to run this file to sync tasks with locations in the database"""
+
+    help = "Creates tasks for locations in the database"
+
+    def handle(self, *args, **kwargs):
+        # fetch locations from the databse
+        locations = Location.objects.using("location_db").all()
+        created_count = 0
+        existing_count = 0
+
+        for location in locations:
+            task, created = Tasks.objects.get_or_create(
+                task_name=f"Task at {location.name}",
+                location_id=location.id,
+                defaults={
+                    "reward": 10,  # default target progress
+                    "target": 100,  # default reward
+                },
+            )
+            if created:
+                created_count += 1
+                self.stdout.write(
+                    self.style.SUCCESS(f"Created task for {location.name}")
+                )
+            else:
+                existing_count += 1
+                self.stdout.write(
+                    self.style.WARNING(f"Task for {location.name} already exists")
+                )
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"\nSummary: {created_count} tasks created, {existing_count} tasks already existed."
+            )
+        )
