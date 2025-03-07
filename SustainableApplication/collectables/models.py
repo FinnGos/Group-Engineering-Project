be5d1f410f04ca858662ab18464ace0c697597ea
ColumXB <92394269+ColumXB@users.noreply.github.com>
2025-03-07 00:15:56 +0000

    Update SustainableApplication/collectables/models.py

--- SustainableApplication/collectables/models.py
+++ SustainableApplication/collectables/models.py
@@ -6,7 +6,6 @@ from django.db import models
 class Collectable(models.Model):
     """Model for in app collectables, storing their names and associated images"""
 
-    #TODO standardise max_length
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to="media/", default="placeholder.jpg")
     fact = models.CharField(max_length=1000, default="")
