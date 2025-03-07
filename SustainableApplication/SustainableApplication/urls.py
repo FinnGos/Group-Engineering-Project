af528e420d5dea4b8e0a85cec099395f3d85df5c
Finn Gosney <fcgosney@gmail.com>
2025-03-07 12:47:16 +0000

    working home page witohut background images

--- SustainableApplication/SustainableApplication/urls.py
+++ SustainableApplication/SustainableApplication/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('TermsAndConditions.urls')),
@@ -31,4 +31,9 @@ urlpatterns = [
     path("home/", include("home.urls")),
     path("leaderboard/", include("leaderboard.urls")),
     path("tasks/", include("tasks.urls")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+# Serve static and media files in development mode
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- SustainableApplication/home/urls.py
+++ SustainableApplication/home/urls.py
@@ -4,6 +4,13 @@ from . import views
 from django.shortcuts import redirect
 from tasks import views as tviews
 from .views import CustomLogoutView, LoginFormView
+from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
+
+# Placeholder views
+def placeholder_view(request):
+    return HttpResponse("This page is under construction.", content_type="text/plain")
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -15,4 +22,7 @@ urlpatterns = [
     path("accounts/update_profile/", views.update_profile, name="update_profile"),
     path("accounts/change_password/", views.change_password, name="change_password"),
     path("accounts/view_data/", views.view_user_data, name="view_user_data"),
-]
+    path("how-to-play/", placeholder_view, name="how_to_play"),
+    path("tasks-view/", placeholder_view, name="tasks_view"),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
