25ef9983874d65eb0cdd87dae4ac1fbce3165473
Kadeem Hannan <kadeemhannan@icloud.com>
2025-03-20 15:12:57 +0000

    Copied collectables folder from lootboxes branch into frontend-leaderboard

---
+++ SustainableApplication/collectables/views.py
@@ -0,0 +1,18 @@
+"""
+Collection of displays for the collectables webpage
+"""
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def collectable_gallery(request):
+    """ Displays page of all cards owned by the user from the collectable database"""
+    user_object = request.user
+    collectables = user_object.collectables_owned.all()
+
+    context = {"collectable_list": collectables}
+    return render(request, "gallery.html", context)
+
+# Create your views here.
