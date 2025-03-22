0a1c39797a1139d4543615b9bc2fb3a0ae44728e
Finn Gosney <fcgosney@gmail.com>
2025-03-22 20:49:42 +0000

    commit

--- SustainableApplication/SustainableApplication/settings.py
+++ SustainableApplication/SustainableApplication/settings.py
@@ -43,10 +43,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 # Allowed hosts
 ALLOWED_HOSTS = ["group-engineering-project.onrender.com", "127.0.0.1"]
 
-CSRF_TRUSTED_ORIGINS = ["https://group-engineering-project.onrender.com/",]
+CSRF_TRUSTED_ORIGINS = ["https://group-engineering-project.onrender.com",]
 SECRET_KEY = os.environ["SECRET_KEY"]
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
 
 # Application definition
 
