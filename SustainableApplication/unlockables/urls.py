2decc987f720b9f622d43924b412c6bb1112f96f
JacobNixon2004 <jake.rob.nixon@gmail.com>
2025-03-24 16:12:50 +0000

    Rubbish fix

--- SustainableApplication/unlockables/urls.py
+++ SustainableApplication/unlockables/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('buy/item/<int:item_id>/', buy_item, name='buy_item'),
     path('shop/', shop, name='shop'),
     path('map/', game_map, name='map'),
-    path("clean-rubbish/<int:rubbish_id>/", clean_rubbish, name="clean_rubbish"),
+    path("clean-rubbish/<int:rubbish_id>", clean_rubbish, name="clean_rubbish"),
 ]
