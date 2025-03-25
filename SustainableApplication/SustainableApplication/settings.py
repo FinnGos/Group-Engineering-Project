f545080686bf938f91315aadbe5f310f44fd0a0e
ColumXB <92394269+ColumXB@users.noreply.github.com>
2025-03-25 11:52:00 +0000

    Update settings.py

--- SustainableApplication/SustainableApplication/settings.py
+++ SustainableApplication/SustainableApplication/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = os.environ["SECRET_KEY"]
-SECRET_KEY = "DSAKGOEKTOWPKEA12315124"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # The URL that handles the static files
 STATIC_URL = "/static/"
